a6bbc2a1bb9f5dbc46d9da738e57a546f53e858e
idlesentry <idlesentry@gmail.com>
2016-02-27 14:59:14 -0800

    Added small list in bottom left corner

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ function initMap() {
   var myLatLng = {lat: 48.748767, lng: -122.477416};
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 18,
+    zoom: 17,
     center: myLatLng
   });
 
@@ -120,3 +120,26 @@ function initMap() {
     }
   }
 }
+
+var BetterListModel = function () {
+    this.itemToAdd = ko.observable("");
+    this.allItems = ko.observableArray(["Fries", "Eggs Benedict", "Ham", "Cheese"]); // Initial items
+    this.selectedItems = ko.observableArray(["Ham"]);                                // Initial selection
+ 
+    this.addItem = function () {
+        if ((this.itemToAdd() != "") && (this.allItems.indexOf(this.itemToAdd()) < 0)) // Prevent blanks and duplicates
+            this.allItems.push(this.itemToAdd());
+        this.itemToAdd(""); // Clear the text box
+    };
+ 
+    this.removeSelected = function () {
+        this.allItems.removeAll(this.selectedItems());
+        this.selectedItems([]); // Clear selection
+    };
+ 
+    this.sortItems = function() {
+        this.allItems.sort();
+    };
+};
+ 
+ko.applyBindings(new BetterListModel());
